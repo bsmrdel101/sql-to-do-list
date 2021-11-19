d959a64d82b2781b8bd955627b6a20798990dae7
Bennett Smrdel <smrdelb@gmail.com>
2021-11-19 10:54:30 -0600

    fixed module not connecting to server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const taskRouter = require('./routes/task.router');
+const taskRouter = require('./routes/taskRouter');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
